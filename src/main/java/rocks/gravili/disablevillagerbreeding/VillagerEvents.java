4413e96887be20850727ecd5c920003bc79726ac
AlessioGr <alessio@gravili.de>
2022-01-21 18:28:25 +0100

    Update

--- src/main/java/rocks/gravili/disablevillagerbreeding/VillagerEvents.java
+++ src/main/java/rocks/gravili/disablevillagerbreeding/VillagerEvents.java
@@ -9,11 +9,10 @@ public class VillagerEvents implements Listener {
 
     @EventHandler
     public void onBreed(EntityBreedEvent e){
-        if(e.getEntity().getType() == EntityType.VILLAGER){
-            if(e.getBreeder() != null && e.getBreeder().hasPermission("disablevillagerbreeding.bypass")){
-                return;
+        if (e.getEntity().getType() == EntityType.VILLAGER || e.getMother().getType() == EntityType.VILLAGER || e.getFather().getType() == EntityType.VILLAGER) {
+            if(e.getBreeder() == null || !e.getBreeder().hasPermission("disablevillagerbreeding.bypass")){
+                e.setCancelled(true);
             }
-            e.setCancelled(true);
         }
     }
 }
